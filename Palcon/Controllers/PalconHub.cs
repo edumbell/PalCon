04d84645d6ed05cc6d396b7a94d024f9a9ba0b78
Ed <no@no.no>
2017-01-31 00:32:36 +0200

    adding solution and packages

---
+++ Palcon/Controllers/PalconHub.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.AspNet.SignalR;
+using Palcon.Models;
+namespace Palcon.Controllers
+{
+    public class PalconHub : Hub
+    {
+        public static object _lock = new object();
+        public void JoinGame()
+        {
+            lock (_lock)
+            {
+                var toJoin = Game.Games.Where(x => !x.Started).FirstOrDefault();
+                if (toJoin == null)
+                {
+                    toJoin = new Game();
+
+                    if (Game.Games.Any())
+                    {
+                        toJoin.GameId = Game.Games.Max(x => x.GameId) + 1;
+                    }
+                    else
+                        toJoin.GameId = 1;
+                    Game.Games.Add(toJoin);
+                }
+                var player = new Player();
+                //if (toJoin.Players.Any())
+                //{
+                //    player.PlayerId = toJoin.Players.Max(x => x.PlayerId) + 1;
+                //}
+                //else
+                //    player.PlayerId = 1;
+                player.ConnectionId = Context.ConnectionId;
+
+                toJoin.Players.Add(player);
+                foreach (var p in toJoin.Players)
+                {
+                    Clients.Client(p.ConnectionId).playerJoined(toJoin.Players.Count());
+                }
+
+                Clients.Client(player.ConnectionId).joinSuccess(toJoin.GameId);
+
+            }
+        }
+
+        public void ClientReadyToStart(int gameId)
+        {
+            var game = Game.Games.Where(x => x.GameId == gameId).Single();
+            var p = game.Players.Where(x => x.ConnectionId == Context.ConnectionId).Single();
+            p.IsReadyToStart = true;
+
+        }
+
+        public void SendSettings(int gameId, string settings)
+        {
+            var game = Game.Games.Where(x => x.GameId == gameId).Single();
+            if (!game.Started)
+            {
+                foreach (var p in game.Players)
+                {
+                    Clients.Client(p.ConnectionId).receiveSettings(settings);
+                }
+            }
+        }
+
+        public int TryStartGame(int gameId)
+        {
+            lock (_lock)
+            {
+                var game = Game.Games.Where(x => x.GameId == gameId).Single();
+                if (!game.Started)
+                {
+                    game.Started = true;
+                    foreach (var p in game.Players)
+                    {
+                        Clients.Client(p.ConnectionId).checkReadyToStart();
+                    }
+                    System.Threading.Thread.Sleep(1000);
+                    game.Players = game.Players.Where(x => x.IsReadyToStart).ToList();
+                    return game.Players.Count();
+                }
+                else
+                    return 0;
+            }
+        }
+
+        public void SendMap(int gameId, string json)
+        {
+            var game = Game.Games.Where(x => x.GameId == gameId).Single();
+            int pid = 0;
+            foreach (var p in game.Players)
+            {
+                pid++;
+                p.PlayerId = pid;
+                Clients.Client(p.ConnectionId).receiveMap(p.PlayerId, json);
+            }
+        }
+
+        public void EndTurn(Game game)
+        {
+            var jsonAll = game.EndTurnAndGetCommands();
+            foreach (var p in game.LivePlayers())
+            {
+                Clients.Client(p.ConnectionId).receiveCommands(jsonAll);
+            }
+        }
+
+        public void SendCommands(int gameId, string json)
+        {
+            var game = Game.Games.Where(x => x.GameId == gameId).Single();
+            var player = game.Players.Where(x => x.ConnectionId == Context.ConnectionId).Single();
+            player.CurrentCommand = json;
+            var allSent = game.AllCommandsIn();
+            if (allSent)
+            {
+                EndTurn(game);
+            }
+            else
+            {
+                var schedule = System.Threading.Tasks.Task.Run(async () => {
+                    await System.Threading.Tasks.Task.Delay(1500);
+                    if (game.TimeLastTurnEnd.AddMilliseconds(1500) < DateTime.Now )
+                    {
+                        EndTurn(game);
+                    }
+                });
+            }
+        }
+
+        
+
+        public void Hello()
+        {
+            Clients.All.hello();
+        }
+    }
+}
---
+++ Palcon/Models/GameManager.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Palcon.Models
+{
+    public class Game
+    {
+        public static List<Game> Games = new List<Game>();
+
+        public int GameId { get; set; }
+        public List<Player> Players { get; set; }
+        public bool Started { get; set; }
+        public int msPerTurn = 500;
+        public int turnId = 0;
+        public DateTime TimeLastTurnEnd { get; set; }
+        public Game()
+        {
+            Players = new List<Player>();
+            TimeLastTurnEnd = DateTime.Now;
+        }
+
+        public List<Player> LivePlayers()
+        {
+            return Players.Where(x => !x.IsDead).ToList();
+        }
+
+        public bool AllCommandsIn()
+        {
+            var alivePlayerCount = LivePlayers().Count();
+            var commandsSent = Players.Where(x => x.CurrentCommand != null).Count();
+            return alivePlayerCount == commandsSent;
+        }
+
+        public string EndTurnAndGetCommands()
+        {
+            turnId++;
+            TimeLastTurnEnd = DateTime.Now;
+            string result = "";
+            foreach (var p in LivePlayers())
+            {
+                if (p.CurrentCommand != null)
+                {
+                    if (p.LagScore > 0)
+                        p.LagScore -= 1;
+                    result += p.CurrentCommand.Trim('[').Trim(']');
+                }
+                else
+                {
+                    p.LagScore += 10;
+                    if (p.LagScore > 100)
+                        p.IsDead = true;
+                }
+                p.CurrentCommand = null;
+            }
+            return "{\"turnId\":" + turnId.ToString() + ",\"commands\": [" + result + "]}";
+        }
+    }
+
+    public class Player
+    {
+        public int GameId { get; set; }
+        public int PlayerId { get; set; }
+        public string Name { get; set; }
+        public bool IsDead { get; set; }
+        public string ConnectionId { get; set; }
+        public bool IsReadyToStart { get; set; }
+        public string CurrentCommand { get; set; }
+        public int LagScore { get; set; }
+    }
+}
